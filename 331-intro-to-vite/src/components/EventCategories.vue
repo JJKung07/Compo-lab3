f8b766b2fda2842ec82448125ef70f14c54a6f71
JJKung <jj10102546@gmail.com>
2024-08-07 23:38:39 +0700

    Finish 2.4

---
+++ 331-intro-to-vite/src/components/EventCategories.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Event from '@/types/Event'
+defineProps<{
+  event: Event
+}>()
+</script>
+
+<template>
+  <div class="category">
+    <div class="category-card">
+      <span>{{ event.category }}</span>
+      <span>{{ event.organizer }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.category-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+  display: flex;
+  justify-content: flex-end;
+  font-size: 16px;
+  text-align: right;
+}
+
+.category-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.category span {
+  margin-left: 10px;
+}
+</style>
---
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+import EventCategory from '@/components/EventCategories.vue'
+import Event from '@/type/Event'
+import { ref, onMounted, computed } from 'vue'
+import EventService from '@/services/EventService'
+const events = ref<Event[]>([])
+
+const props = defineProps({
+  page: {
+    type: Number,
+    required: true
+  }
+})
+const page = computed(() => props.page)
+onMounted(() => {
+  EventService.getEvents(2, page.value)
+    .then((response) => {
+      events.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <!-- new element -->
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <EventCategory v-for="event in events" :key="`cat-org-${event.id}`" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
