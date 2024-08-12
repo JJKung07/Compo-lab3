7cd445375b3a7159593134f2c31341d896da9de6
JJKung <jj10102546@gmail.com>
2024-08-12 21:49:38 +0700

    Fix student bug

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView} from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router'
 import { useMessageStore } from '@/stores/message'
 import { storeToRefs } from 'pinia'
 const store = useMessageStore()
@@ -19,11 +19,19 @@ const pageSize = ref<number>(2) // Default page size
       <h1>Deploy with Vercel</h1>
       <div class="wrapper">
         <nav class="py-6 pl-6">
-          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view', query: { page: 1, size: pageSize } }"
+          <RouterLink
+            class="font-bold text-gray-700"
+            exact-active-class="text-green-500"
+            :to="{ name: 'event-list-view', query: { page: 1, size: pageSize } }"
             >Event</RouterLink
           >
-          | <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'about' }">About</RouterLink> |
-          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'student' }">Student</RouterLink>
+          |
+          <RouterLink
+            class="font-bold text-gray-700"
+            exact-active-class="text-green-500"
+            :to="{ name: 'about' }"
+            >About</RouterLink
+          >
         </nav>
       </div>
     </header>
